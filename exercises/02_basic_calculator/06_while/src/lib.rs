019c182dfc0d59b4a1ea24e5019b481cd8d071ef
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2024-07-02 15:07:43 +0900

    2.6

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result: u32 = 1;
+    let mut counter: u32 = n;
+    while counter > 0 {
+        result *= counter;
+        counter -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
